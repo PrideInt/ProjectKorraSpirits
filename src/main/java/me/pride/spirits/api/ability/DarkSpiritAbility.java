d0ec0461e15aeff9ec1a912759eac201fa561e04
Pride <20098992+PrideInt@users.noreply.github.com>
2024-05-28 04:30:00 -0400

    DarkSpiritAbility and LightSpiritAbility should extend SpiritAbility

--- src/main/java/me/pride/spirits/api/ability/DarkSpiritAbility.java
+++ src/main/java/me/pride/spirits/api/ability/DarkSpiritAbility.java
@@ -1,11 +1,10 @@
 package me.pride.spirits.api.ability;
 
 import com.projectkorra.projectkorra.Element;
-import com.projectkorra.projectkorra.ability.ElementalAbility;
 import me.pride.spirits.api.SpiritType;
 import org.bukkit.entity.Player;
 
-public abstract class DarkSpiritAbility extends ElementalAbility {
+public abstract class DarkSpiritAbility extends SpiritAbility {
 	public DarkSpiritAbility(Player player) {
 		super(player);
 	}
--- src/main/java/me/pride/spirits/api/ability/LightSpiritAbility.java
+++ src/main/java/me/pride/spirits/api/ability/LightSpiritAbility.java
@@ -1,11 +1,10 @@
 package me.pride.spirits.api.ability;
 
 import com.projectkorra.projectkorra.Element;
-import com.projectkorra.projectkorra.ability.ElementalAbility;
 import me.pride.spirits.api.SpiritType;
 import org.bukkit.entity.Player;
 
-public abstract class LightSpiritAbility extends ElementalAbility {
+public abstract class LightSpiritAbility extends SpiritAbility {
 	public LightSpiritAbility(Player player) {
 		super(player);
 	}
